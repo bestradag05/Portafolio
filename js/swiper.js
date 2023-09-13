a6a5d3bea878571e31e669b8b6e1d1ecda421ea5
bestradag05 <bestradag05@gmail.com>
2023-09-13 14:29:29 -0500

    swiper

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+  });
