054fca07e40ed67af9889ac27ec382bc778c6f8d
bestradag05 <bestradag05@gmail.com>
2023-09-14 00:08:02 -0500

    Avance proyectos

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,28 @@
 var swiper = new Swiper(".mySwiper", {
-    centeredSlides: true,
-    slidesPerView: 3,
-    spaceBetween: 50,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-
-  });
+  effect: "coverflow",
+  centeredSlides: true,
+  slidesPerView: 3,
+  spaceBetween: 50,
+  initialSlide: 0,
+  autoHeight: true,
+  centeredSlidesBounds: true,
+  preventClicks: true,
+  loop: true,
+  zoom: true,
+  autoHeight: true,
+  hideOnClick: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    modifier: 1,
+    slideShadows: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
