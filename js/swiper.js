bf6aee023efac885d120099c772929ab413caa74
bestradag05 <bestradag05@gmail.com>
2023-09-15 14:51:42 -0500

    Portafolio responsive desing

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,24 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+
+  },
   centeredSlides: true,
-  slidesPerView: 3,
-  spaceBetween: 50,
   initialSlide: 0,
   loop: true,
   loopedSlides: 2,
